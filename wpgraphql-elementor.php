3e946bb4bd75277e9042794c5a54217fa9d18850
Alejandro <a.chataing.a@gmail.com>
2021-02-18 00:41:11 -0400

    New commit

--- wpgraphql-elementor.php
+++ wpgraphql-elementor.php
@@ -115,7 +115,7 @@ function register_my_custom_graphql_field() {
 		'resolve' => function( $post ) {
 
 			// Let's get the content of post number 123
-			$response = wp_remote_get( "http://localhost/index.php/elementor-11/" );
+			$response = wp_remote_get( "https://google.com" );
 		
 			if ( is_array( $response ) ) {
 		
