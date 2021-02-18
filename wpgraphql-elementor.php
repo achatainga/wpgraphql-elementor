5f4d01ac1f5b137b677a2dc8c39b57caf29c7cce
Alejandro <a.chataing.a@gmail.com>
2021-02-18 12:38:54 -0400

    New commit

--- wpgraphql-elementor.php
+++ wpgraphql-elementor.php
@@ -159,6 +159,7 @@ function wpgraphql_elementor_get_content($URL){
 	// curl_setopt($ch, CURLOPT_STDERR, "log.txt");
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
 
 	curl_setopt($ch, CURLOPT_URL, $URL);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
